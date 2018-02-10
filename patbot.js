026bbf50dea3268f2105d07e6974dd96d30ec167
Patrick <patrick.peters@aboutyou.de>
2018-02-11 00:32:07 +0100

    Added /options in terms of markup buttons like /joke

--- patbot.js
+++ patbot.js
@@ -1,15 +1,13 @@
 const TeleBot = require('telebot');
+const curl = require('curlrequest');
+
 const answer = require('./answers.js');
 
-const bot = new TeleBot('');
+const bot = new TeleBot('TELEGRAM_TOKEN');
 const markdownOption = { parseMode: 'Markdown' };
 
 bot.on('/start', msg => {
     return bot.sendMessage(msg.from.id, answer.start(msg.from), markdownOption);
-});
-
-bot.on('text', msg => {
-    return bot.sendMessage(msg.from.id, answer.echo(msg.text), markdownOption);
 });
 
 bot.on('sticker', (msg) => {
@@ -19,5 +17,53 @@ bot.on('sticker', (msg) => {
 bot.on('/stop', msg => {
     return bot.sendMessage(msg.from.id, answer.stop(msg.from), markdownOption);
 });
+
+// ------------------------------  KEYBOARD ------------------------------
+
+// On commands
+bot.on(['/options'], msg => {
+
+    let replyMarkup = bot.keyboard([
+        ['/buttons', '/joke'],
+        ['/start', '/hide']
+    ], {resize: true});
+
+    return bot.sendMessage(msg.from.id, 'Keyboard.', {replyMarkup});
+
+});
+
+// Buttons
+bot.on('/buttons', msg => {
+
+    let replyMarkup = bot.keyboard([
+        [bot.button('contact', 'Your contact'), bot.button('location', 'Your location')],
+        ['/options', '/hide']
+    ], {resize: true});
+
+    return bot.sendMessage(msg.from.id, 'Button example.', {replyMarkup});
+
+});
+
+bot.on('/joke', (msg) => {
+    var options = { url: 'icanhazdadjoke.com', headers: { accept: 'text/plain' } };
+
+    curl.request(options, function (err, data) {
+        return bot.sendMessage(msg.from.id, data, markdownOption);
+    });
+});
+
+// Hide keyboard
+bot.on('/hide', msg => {
+    return bot.sendMessage(
+        msg.from.id, 'Hide keyboard. Type /options to show.', {replyMarkup: 'hide'}
+    );
+});
+
+// On location on contact message
+bot.on(['location', 'contact'], (msg, self) => {
+    return bot.sendMessage(msg.from.id, `Thank you for ${ self.type }.`);
+});
+
+// -----------------------------------------------------------------------
 
 bot.connect();
